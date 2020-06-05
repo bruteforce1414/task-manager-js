f57546bffdeaac4c6e6b3b11d24e5d1782ef75be
bruteforce1414 <bruteforce1414@gmail.com>
2020-06-05 11:44:43 +0500

    feat: tasks add successfully to frontend

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -14,7 +14,39 @@ const AddTodo = ({ dispatch }) => {
             return
           }
           dispatch(addTodo(input.value))
-          input.value = ''
+
+          fetch("http://localhost:8080/v1/tasks", {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              title: input.value,
+              description: "",
+              date: new Date(),
+              shared: true,
+              status: "To Do",
+            }),
+
+          })	.then(async function(data) {
+          let commit = await data.json();
+            const addedTask = {
+              id: commit.id,
+              title: commit.title,
+              date: commit.date,
+              status: commit.status,
+            };
+          /*   items.push(addedTask)
+            console.log("COMMIT", commit)
+            self.setState({items:items}) */
+          })
+            .catch(err=>console.log("error", err));
+
+
+            input.value = ''
+
+
         }}
       >
         <input ref={node => (input = node)} />
